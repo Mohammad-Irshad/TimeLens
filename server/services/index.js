604d31931160ef723c309397a243c1feb29d5726
Mohammad-Irshad <mohammadirshadcse@gmail.com>
2025-03-20 16:20:04 +0530

    update services

--- server/services/index.js
+++ server/services/index.js
@@ -1,9 +1,12 @@
-// helper function
+// Updated helper function
 
 function setSecureCookie(res, token, name) {
   res.cookie(name, token, {
-    httpOnly: true,
-    maxAge: 60 * 1000,
+    httpOnly: true, // Prevent access to cookie from JS
+    secure: true, // Required for HTTPS
+    sameSite: "None", // Cross-site cookie requirement
+    path: "/", // Make the cookie available across all paths
+    maxAge: 60 * 60 * 1000, // 1 hour expiration time (changed from 1 minute)
   });
 
   return res;
